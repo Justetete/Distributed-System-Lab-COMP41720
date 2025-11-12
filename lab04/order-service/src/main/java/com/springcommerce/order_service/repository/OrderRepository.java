ea5b97ba6bb88e07c991929a242a6a18e369b0b9
Xinchi Jian <justeriam@gmail.com>
2025-11-12 21:56:04 +0000

    clear the old data

--- lab04/order-service/src/main/java/com/springcommerce/order_service/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springcommerce.order_service.repository;
-
-import com.springcommerce.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- lab04/product-service/src/main/java/com/springcommerce/product_service/dto/ProductRequest.java
+++
@@ -1,8 +0,0 @@
-package com.springcommerce.product_service.dto;
-
-
-import java.math.BigDecimal;
-
-public record ProductRequest(String id, String name, String description, BigDecimal price) {
-
-}
